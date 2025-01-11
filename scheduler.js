b06dcdf858667f608656e2bb3f278ebe35e0216b
Namangupta2125 <116955157+Namangupta2125@users.noreply.github.com>
2025-01-11 16:47:31 +0530

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,35 +1,41 @@
-const cloudscraper = require("cloudscraper");
-const path = require("path");
-const node_cron = require("node-cron");
-const coinModel = require(path.join(__dirname, "Models", "coinModel.js"));
-require("dotenv").config();
+const path = require("path")
+const node_cron= require('node-cron')
+const coinModel = require(path.join(__dirname,"Models",'coinModel.js'))
+require('dotenv').config()
 
+
+// url for fetch
 const url = process.env.QUERY_URI;
 
-const fetchAndStore = async () => {
-  try {
-    const resp = await cloudscraper.get(url, { json: true });
 
-    resp.forEach(async (data) => {
-      let savingName = data.name.toLowerCase();
+// fetch data and store it in database for node-cron
+const fetchAndStore = async ()=>{
+    try{
+        const resp = (await (await fetch(url)).json());
+        
+        resp.forEach(async (data)=>{
+            let savingName = data.name.toLowerCase();
 
-      // the name of matic is polygon in the API. handling that
-      if (savingName === "polygon") {
-        savingName = "matic";
-      }
-      const coinData = new coinModel({
-        name: savingName,
-        price: parseFloat(data.current_price, 10),
-        marketCap: parseFloat(data.market_cap, 10),
-        change24h: parseFloat(data.price_change_24h, 10),
-      });
-      await coinData.save();
-    });
-  } catch (error) {
-    console.error("An error occurred: ", error.message);
-  }
-};
+            // the name of matic is polygon in the api. handling that
+            if(savingName === "polygon"){
+                savingName = "matic"
+            }
+            const coinData = new coinModel({ 
+              name: savingName,
+              price: parseFloat(data.current_price, 10),
+              marketCap: parseFloat(data.market_cap, 10),
+              change24h: parseFloat(data.price_change_24h, 10),
+            });
+            await coinData.save();  
+        })
+    }
+    catch(error)
+    {
+        console.log('An error occurred ',error);
+    }
+}
 
-module.exports.schedulerInit = () => {
-  node_cron.schedule("* * * * *", fetchAndStore);
-};
+
+
+// setting the time limit * * * * * => min hours day month day
+module.exports.schedulerInit = ()=>{node_cron.schedule("* * * * *",fetchAndStore);}
