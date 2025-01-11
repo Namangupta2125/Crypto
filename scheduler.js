3a8d3407a307963f3493627488f5598d73abdcb6
Namangupta2125 <116955157+Namangupta2125@users.noreply.github.com>
2025-01-11 16:29:34 +0530

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,4 @@
+const cloudscraper = require("cloudscraper");
 const path = require("path");
 const node_cron = require("node-cron");
 const coinModel = require(path.join(__dirname, "Models", "coinModel.js"));
@@ -7,42 +8,28 @@ const url = process.env.QUERY_URI;
 
 const fetchAndStore = async () => {
   try {
-    const response = await fetch(url, {
-     headers: {
-    "Accept": "application/json",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-    "Referer": "https://www.coingecko.com",
-    "Origin": "https://www.coingecko.com"
-  }
-
-    });
-
-    if (!response.ok) {
-      console.error(`HTTP error! Status: ${response.status}`);
-      console.log(await response.text());
-      return;
-    }
-
-    const resp = await response.json();
+    const resp = await cloudscraper.get(url, { json: true });
 
     resp.forEach(async (data) => {
       let savingName = data.name.toLowerCase();
-      if (savingName === "polygon") savingName = "matic";
 
+      // the name of matic is polygon in the API. handling that
+      if (savingName === "polygon") {
+        savingName = "matic";
+      }
       const coinData = new coinModel({
         name: savingName,
         price: parseFloat(data.current_price, 10),
         marketCap: parseFloat(data.market_cap, 10),
         change24h: parseFloat(data.price_change_24h, 10),
       });
-
       await coinData.save();
     });
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error("An error occurred: ", error.message);
   }
 };
 
 module.exports.schedulerInit = () => {
-  node_cron.schedule("* */2 * * *", fetchAndStore);
+  node_cron.schedule("* * * * *", fetchAndStore);
 };
